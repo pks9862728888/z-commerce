7345cc5cc96e3fad9779464763985376b623429f
pks9862728888 <pks9862728888@gmail.com>
2024-03-04 23:32:33 +0530

    Initial commit

---
+++ src/main/java/com/demo/zcommerce/ZcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.zcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/zcommerce/ZcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.zcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
